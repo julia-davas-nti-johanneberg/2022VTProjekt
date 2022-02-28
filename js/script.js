f781585ac57256e618741256f5a9dced46a9d471
julia-davas-nti-johanneberg <94617563+julia-davas-nti-johanneberg@users.noreply.github.com>
2022-02-28 10:10:22 +0100

    lektion mån 28feb
    shrinking header och hamburgemeny!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+    document.querySelector('.navLinks').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
+}
+
+document.querySelector('.burger').addEventListener('click', toggleMeny)
